f7708d0589c7fb7dc19e8474d6664bd2ab4052bd
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-10-27 21:51:11 +0300

    Add init dirs

---
+++ necropolis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  necropolis
+//
+//  Created by Иван Коваленко on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
